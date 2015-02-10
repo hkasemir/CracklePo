373ced79ed21b74da0e20e94d6796f1509479b60
Heidi Kasemir <heidi.kasemir@gmail.com>
2015-02-10 13:21:33 -0800

    Create CracklePop.py
    code to count to 100 with crackles and pops

---
+++ CracklePop.py
@@ -0,0 +1,11 @@
+for  i in range(1,101):
+  if i %3 == 0:
+    if i %5 == 0:
+      print("cracklepop")
+    else:
+      print("crackle")
+  elif i %5 == 0:
+    print ("pop")
+  else:
+    print (i)
+      
